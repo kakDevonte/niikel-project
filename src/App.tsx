9121c7c786a5e38e0046723b07592f67ba78f060
HatyushinAS <kakdevonte@yandex.ru>
2022-07-11 12:23:33 +0300

    add app.scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Header } from './components/Header';
 import './scss/app.scss';
+import { PatientTable } from './components/PatientTable';
 
 function App() {
   return (
     <div>
       <Header />
+      <div className="wrapper">
+        <PatientTable />
+      </div>
     </div>
   );
 }
---
+++ src/components/PatientTable/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const PatientTable: React.FC = () => {
+  return (
+    <>
+      <table>
+        <td>Бла</td>
+        <td>Бла</td>
+        <tbody>
+          <tr>asd</tr>
+          <tr>asd</tr>
+        </tbody>
+      </table>
+    </>
+  );
+};
